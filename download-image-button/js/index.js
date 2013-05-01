6242e080d61c82c48768aea7377358933ea2b07c
Kristian Dimitrov <kristian.dimitrov.sz@gmail.com>
2013-05-01 07:15:31 -0500

    Created sample

--- download-image-button/js/index.js
+++ download-image-button/js/index.js
@@ -1,50 +1,69 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
+    var that = this,
+        App = new downloadApp();
+    
+    navigator.splashscreen.hide();
+    App.run();
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+var downloadApp = function() {
+}
+
+downloadApp.prototype = {
+	run: function() {
+		var that = this;
+        
+		document.getElementById("download").addEventListener("click", function() {
+			that.getFilesystem(
+				function(fileSystem) {
+                    
+					console.log("gotFS");
+                    
+					if (device.platform === "Android") {
+						that.getFolder(fileSystem, "test", function(folder) {
+							that.transferFile(folder.fullPath)
+						}, function() {
+							console.log("failed to get folder");
+						});
+					} else {
+                        that.transferFile(fileSystem.root.fullPath)
+                    }
+				},
+				function() {
+					console.log("failed to get filesystem");
+				}
+				);
+		});
+	},
+    
+	getFilesystem:function (success, fail) {
+		window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, success, fail);
+	},
+
+	getFolder: function (fileSystem, folderName, success, fail) {
+		fileSystem.root.getDirectory(folderName, {create: true, exclusive: false}, success, fail)
+	},
+
+	transferFile: function (fileSystemPath) {
+		var transfer = new FileTransfer(),
+		uri = encodeURI("http://www.icenium.com/assets/img/icenium-logo.png");
+		filePath = fileSystemPath + '/sample.png';
+		transfer.download(
+			uri,
+			filePath,
+			function(entry) {
+				var image = document.getElementById("downloadedImage");
+				image.src = entry.fullPath;
+                document.getElementById("result").innerHTML = "File saved to: " + entry.fullPath;
+			},
+			function(error) {
+				console.log("download error source " + error.source);
+				console.log("download error target " + error.target);
+				console.log("upload error code" + error.code);
+			}
+			);
+	}
+}
